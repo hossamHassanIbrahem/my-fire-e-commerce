2d9c6cf3003f41629e8b4d26e43d20a10a79e514
ahmed <ahmed.ali.ibrahim111@gmail.com>
2023-03-31 18:07:41 +0200

    login , register and forget password pages desgin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ProjFooter from "./Layouts/Footer/Footer";
 import Header from "./Layouts/Header/Header";
+import ForgotPassword from "./Pages/ForgotPassword/forgotPassword";
+import Login from "./Pages/Login/login";
+import Register from "./Pages/Register/register";
 // require('dotenv').config()
 function App() {
   return (
     <div className="App container-fluid" >
       <Header />
+
+      <Routes>
+      {/* <Route path='/' element={<Login/>} >  </Route> */}
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/forgotpassword" element={<ForgotPassword/>} />
+
+    </Routes>
       <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
       <ProjFooter />
     </div>
