2465b98bde6686d51bb4950812740ab19f7f367d
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2023-04-01 10:02:43 +0200

    Routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import "./App.css";
 import ProjFooter from "./Layouts/Footer/Footer";
 import Header from "./Layouts/Header/Header";
+//// F ////
+import UserAccount from './pages/user-account/user-account';
+import { Route, Routes } from 'react-router-dom';
+import MyAccount from './component/myAccount/myAccount';
+import MyOrders from './component/myOrders/myOrders';
+import MyWishList from './component/myWishList/myWishList';
+import AddressBook from './component/addressBook/addressBook';
+import AccountInformation from './component/accountInformation/accountInformation';
+/////
+
 // require('dotenv').config()
 function App() {
   return (
     <div className="App container-fluid" >
       <Header />
       <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<Routes>
+          <Route path='/useraccount' element={<UserAccount />}>
+
+            <Route index element={<MyAccount/>}/>
+            <Route  path='myaccount' element={<MyAccount/>}/>
+            <Route  path='myorders' element={<MyOrders/>}/>
+            <Route  path='mywishlist' element={<MyWishList/>}/>
+            <Route path='addressbook' element={<AddressBook />}/>
+            <Route  path='accountinfo' element={<AccountInformation/>}/>
+
+
+          </Route>
+
+
+
+        </Routes>
+
+
       <ProjFooter />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -7,9 +7,17 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
+   <React.StrictMode>
+    <BrowserRouter>
+
+    <Provider store={store}>
     <App />
+    </Provider>
+    
+    </BrowserRouter>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
