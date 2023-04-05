93f4c64fb6cacabb0aa06150a49cd74f9fa789c0
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-04-05 17:06:53 +0200

    fdf
    fdfd

--- src/index.js
+++ src/index.js
@@ -5,40 +5,40 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-<<<<<<< HEAD
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// <<<<<<< HEAD
 
    <React.StrictMode>
-    <BrowserRouter>
+
 
     <Provider store={store}>
-=======
-  <BrowserRouter>
-  <React.StrictMode>
->>>>>>> marina_samy
-    <App />
+{/* ======= */}
     </Provider>
+  <BrowserRouter>
+{/* >>>>>>> marina_samy */}
+  <React.StrictMode>
+    <App />
     
-    </BrowserRouter>
-  </React.StrictMode>
-<<<<<<< HEAD
 
-=======
+  </React.StrictMode>
+{/* <<<<<<< HEAD */}
+
+{/* ======= */}
   <BrowserRouter>
     <App />
   </BrowserRouter>
->>>>>>> ahmed-ali
-=======
+{/* >>>>>>> ahmed-ali */}
+{/* ======= */}
   </BrowserRouter>
->>>>>>> marina_samy
+{/* >>>>>>> marina_samy */}
 );
 
-// If you want to start measuring performance in your app, pass a function
+{/* // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals */}
 reportWebVitals();
